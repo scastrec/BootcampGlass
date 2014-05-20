56983d233075b6eacab393d380ca517e6f31d43f
Stephane <stephane.castrec@gmail.com>
2014-05-20 16:34:42 +0200

    navigating between cards

--- app/src/main/java/com/stephanecastrec/helloglass/app/MainActivity.java
+++ app/src/main/java/com/stephanecastrec/helloglass/app/MainActivity.java
@@ -4,14 +4,79 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.FrameLayout;
+import android.widget.Toast;
+
+import com.google.android.glass.app.Card;
+import com.google.android.glass.touchpad.Gesture;
+import com.google.android.glass.touchpad.GestureDetector;
+import com.google.android.glass.widget.CardScrollView;
+import com.stephanecastrec.helloglass.app.adapter.MyCardScrollAdapter;
+
+import java.util.ArrayList;
 
 
 public class MainActivity extends Activity {
 
+    private ArrayList<Card> mCards;
+    private CardScrollView mCardScrollView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        createCards();
+
+        mCardScrollView = new CardScrollView(this);
+        MyCardScrollAdapter adapter = new MyCardScrollAdapter(mCards);
+        mCardScrollView.setAdapter(adapter);
+        mCardScrollView.activate();
+        setContentView(mCardScrollView);
+        mCardScrollView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                tappedCard();
+            }
+        });
+    }
+
+    /**
+     * Create cards to display.
+     */
+    private void createCards() {
+        mCards = new ArrayList<Card>();
+
+        Card card;
+
+        card = new Card(this);
+        card.setText("Welcome to bootcamp");
+        card.setFootnote("swipe to see next cards");
+        mCards.add(card);
+
+        card = new Card(this);
+        card.setText("Do you like this?");
+        card.setFootnote("swipe again!");
+        card.setImageLayout(Card.ImageLayout.FULL);
+        card.addImage(R.drawable.android_logo_small);
+        mCards.add(card);
+
+        card = new Card(this);
+        card.setText("This card has a mosaic of puppies.");
+        card.setFootnote("you can go back in your swipe!!");
+        card.setImageLayout(Card.ImageLayout.LEFT);
+        card.addImage(R.drawable.ic_launcher);
+        card.addImage(R.drawable.android_logo_small);
+        mCards.add(card);
+    }
+
+    /**
+     * Action to do when you have a card tapped.
+     */
+    private void tappedCard() {
+        int i = mCardScrollView.getSelectedItemPosition();
+        Toast.makeText(this, this.getString(R.string.card_tapped) + i, Toast.LENGTH_SHORT).show();
     }
 
 
